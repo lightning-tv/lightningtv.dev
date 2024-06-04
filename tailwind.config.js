b7b9d90f2ed65ae667164c072d490027204310a9
Chris Lorenzo <chiefcll@gmail.com>
2024-06-04 09:30:08 -0400

    update site, more tweak to come

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lightningtv': '#6F45E8',
+        'lightningtv-dark': '#1C64F2',
+      }
+    },
   },
   plugins: [],
 }
